fd2f2bcfc218ef6ba31b734488d7dd8c7c2f8d7b
Juan Pinazo <pinazo@gmail.com>
2011-11-23 17:57:14 -0200

    Initial directory structure and Base64 proof of concept.

---
+++ poc/base64/test-base64.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+var fs = require('fs');
+var path = require('path');
+var assert = require('assert');
+
+var imageData = fs.readFileSync(path.join(process.cwd(), 'actminds.jpg'), 'binary');
+var base64Data = fs.readFileSync('actminds.b64').toString();
+var dataBuffer = new Buffer(base64Data, 'base64').toString('binary');
+
+assert.ok(dataBuffer === imageData);
+
+// Uncomment the line below if you want to see an output of the conversion from base64 data to a JPG image
+// fs.writeFileSync('out.jpg', dataBuffer, encoding='binary')
