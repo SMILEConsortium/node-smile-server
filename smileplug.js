2b903432ea8d4df389cc64a1b0a62c9531cb8be5
David J. Kordsmeier <dkords@gmail.com>
2012-12-31 09:59:08 -0800

    Add quickstart guide, add links to it and the Plugmin web ui.  Bump version to 0.2.22

--- smileplug.js
+++ smileplug.js
@@ -36,7 +36,7 @@ var starttime = (new Date()).getTime();
 js.CONFIG = {
   'PORT' : 80,
   'HOST' : '0.0.0.0',
-  'VERSION_TAG' : '0.2.21',
+  'VERSION_TAG' : '0.2.22',
   'VERSION_DESCRIPTION' : 'SMILE Server',
 };
 
