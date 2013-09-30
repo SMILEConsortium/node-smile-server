e74499a762ba08034d4758c21c6123c13f189774
David J. Kordsmeier <dkords@gmail.com>
2013-09-29 23:30:47 -0700

    refs #26 Add persisteus test cases

--- tests/unit/persisteus.js
+++ tests/unit/persisteus.js
@@ -3,8 +3,18 @@ var assert = require('assert');
 var Persisteus = require('../../lib/smile/persisteus').Persisteus;
 
 exports.testPersisteusConstructor = function(test) {
-    test.expect(1);
+    test.expect(3);
     var aPersisteus = new Persisteus();
     test.equal(true, aPersisteus !== null);
+    test.ok(aPersisteus.DB != null);
+    test.ok(aPersisteus.CONFIG != null);
+    test.done();
+};
+
+exports.testGetInfo = function(test) {
+    test.expect(2);
+    var aPersisteus = new Persisteus();
+    test.equal(true, aPersisteus !== null);
+    test.ok(aPersisteus.getInfo() == null);
     test.done();
 };
