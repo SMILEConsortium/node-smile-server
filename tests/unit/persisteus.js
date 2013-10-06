741ba12eee606a96d79f1d4468a18abab321c02e
David J. Kordsmeier <dkords@gmail.com>
2013-10-06 16:42:14 -0700

    refs #26 A functioning map for returning all sessions.

--- tests/unit/persisteus.js
+++ tests/unit/persisteus.js
@@ -1,7 +1,8 @@
 var assert = require('assert');
+var vows = require('vows');
 
 var Persisteus = require('../../lib/smile/persisteus').Persisteus;
-
+var persisteus;
 var sessiondata = {
     "NAME": "test2",
     "Q": "asdfgh",
@@ -13,15 +14,59 @@ var sessiondata = {
     "O1": "a",
     "TYPE": 'QUESTION'
 };
+var suite = vows.describe('Tests "IQ Data Persistence Local/Remote Online/Offline Using Persisteus.js"');
 
-exports.testPersisteusConstructor = function(test) {
-    test.expect(3);
-    var aPersisteus = new Persisteus();
-    test.equal(true, aPersisteus !== null);
-    test.ok(aPersisteus.DB != null);
-    test.ok(aPersisteus.CONFIG != null);
-    test.done();
-};
+suite.addBatch({
+	"Verify Contructor" : function() {
+		persisteus = new Persisteus();
+	}
+});
+
+
+suite.addBatch({
+    "Verify the persisteus object" : {
+        topic : new(Persisteus),
+        'Exists': function (pers) {
+            assert.isTrue (pers != null);
+        },
+        'Has CONFIG': function (pers) {
+            assert.isTrue (pers.CONFIG != null);
+        },
+        'Exists': function (pers) {
+            assert.isTrue (pers != null);
+        }
+    }
+});
+
+/*
+suite.addBatch({
+    "Verify CRUD for Sessions" : {
+        topic : pers,
+        'Exists': function (pers) {
+        	aPersisteus.putSession(sessiondata, function callback(err, data) { console.log('putSession success'); })
+            assert.isTrue (pers != null);
+        },
+        'Has CONFIG': function (pers) {
+            assert.isTrue (pers.CONFIG != null);
+        },
+        'Exists': function (pers) {
+            assert.isTrue (pers != null);
+        }
+    }
+});
+
+suite.addBatch({
+    "Verify Utility functions" : {
+        topic : new(Persisteus),
+        'Exists': function (pers) {
+            assert.isTrue (pers.logInfo() == null);
+        }
+    }
+});
+
+*/
+/*
+
 
 // XXX This sucks for testing ... switch to vows or something else
 exports.testMethodCalls = function(test) {
@@ -33,3 +78,5 @@ exports.testMethodCalls = function(test) {
     test.ok(aPersisteus.exportDocs('/tmp/foo.json', function callback(err, data) { console.log('export success'); }) == null);
     test.done();
 };
+*/
+suite.run();
